f10b1907360ab26cb8c9c939542f0a4b47ad3e7d
Boucanier <118428520+Boucanier@users.noreply.github.com>
2023-02-10 23:15:05 +0100

    All multiple courses are now detected and it's written on the schedule

--- scrapper.py
+++ scrapper.py
@@ -20,11 +20,14 @@ class Course :
         self.startMinutes = self.toMinutes()
         self.endMinutes = self.startMinutes + self.duration
 
-        self.compatible = True
+        self.sameTime = []
 
     def __str__(self) -> str:
+        st = ''
+        for e in self.sameTime :
+            st += ' ' + str(e)
         display = str(self.weekContent) + ' ' + str(self.dayContent) + ' [' + self.timeContent[0] + ':' + self.timeContent[1] + '] - [' + str(self.startMinutes) + ':' + str(self.endMinutes) + '] - ' + str(self.duration) + ', ' \
-            + self.moduleContent + ' ' + self.roomContent + ' ' + self.profContent + ' ' + self.groupContent
+            + self.moduleContent + ' ' + self.roomContent + ' ' + self.profContent + ' ' + self.groupContent + ', incompatible with :' + st
         return display
 
     def dTime(self) -> int :
@@ -39,11 +42,13 @@ class Course :
         return (int(hr[0])*60) + int(hr[-1])
     
     def isCompatible(self, hr2):
-        if (self.weekContent == hr2.weekContent) and (self.dayContent == hr2.dayContent) and hr2.compatible :
+        if (self.weekContent == hr2.weekContent) and (self.dayContent == hr2.dayContent) :
             if ((self.startMinutes <= hr2.startMinutes < self.endMinutes) or (self.startMinutes < hr2.endMinutes <= self.endMinutes)):
                 return False
-            # if ((hr2.startMinutes <= self.startMinutes < hr2.endMinutes) or (hr2.startMinutes < self.endMinutes <= hr2.endMinutes)):
-            #     return False
+            if ((hr2.startMinutes <= self.startMinutes < hr2.endMinutes) or (hr2.startMinutes < self.endMinutes <= hr2.endMinutes)):
+                return False
+            if (hr2.startMinutes == self.startMinutes) and (hr2.endMinutes == self.endMinutes):
+                return False
         return True
 
 
@@ -139,8 +144,81 @@ def sortCourse(courseList : list):
             courseW2.append(e)
         elif e.weekContent == 3 :
             courseW3.append(e)
+    
+    courseList = [courseW0, courseW1, courseW2, courseW3]
+    
+    for e in courseList :
+        e = checkMultiple(e)
+    
+    overCourse = [[],[],[],[]]
+    overIndex = [[],[],[],[]]
 
-    return courseW0, courseW1, courseW2, courseW3
+    for i in range(len(courseList)):
+        res = multipleSort(courseList[i])
+        courseList[i] = res[0]
+        overCourse[i] = res[1]
+        overIndex[i] = res[2]
+
+    return courseList, overCourse, overIndex
+
+
+def multipleSort(courseList) :
+    tempindex = []
+    temp2index = []
+    replaceCourse = []
+    nComp = []
+    gComp = []
+    for e in courseList :
+        if len(e.sameTime) > 0 :
+            tempindex.append(e.sameTime)
+    for e in tempindex:
+        e.sort()
+        for l in e :
+            if e.count(l) > 1 :
+                e.remove(l)
+    for e in tempindex :
+        if tempindex.count(e) > 1 :
+            tempindex.remove(e)
+    if (len(tempindex) > 1) :
+        for i in range(len(tempindex)):
+            for j in range(len(tempindex)):
+                if (i != j) and all(e in tempindex[i] for e in tempindex[j]) and tempindex[i]!=tempindex[j]:
+                    temp2index.append(tempindex[j])
+        tempindex = [e for e in tempindex if e not in temp2index]
+        for i in range(len(tempindex)):
+            for j in range(len(tempindex[i])):
+                for k in range(len(tempindex)):
+                    if (i!=k) and tempindex[i][j] in tempindex[k]:
+                        [tempindex[k].append(e) for e in tempindex[i] if e not in tempindex[k]]
+        for e in tempindex :
+            for k in e :
+                if e.count(k) != 1:
+                    e.remove(k)
+        for e in tempindex :
+            e.sort()
+        for e in tempindex :
+            if tempindex.count(e) != 1 :
+                tempindex.remove(e)
+    for e in tempindex :
+        tempstart = 1140
+        tempend = 0
+        hstart = ''
+        hend = ''
+        for k in e :
+            if courseList[k].startMinutes < tempstart :
+                tempstart = courseList[k].startMinutes
+                hstart = courseList[k].timeContent[0]
+            if courseList[k].endMinutes > tempend :
+                tempend = courseList[k].endMinutes
+                hend = courseList[k].timeContent[1]
+        replaceCourse.append(Course(courseList[e[0]].dayContent, [hstart,hend], "COURS", "- - -", "MULTIPLES", "- - -", courseList[e[0]].weekContent))
+
+    for e in courseList:
+        if len(e.sameTime) != 0 :
+            nComp.append(e)
+    gComp = [e for e in courseList if e not in nComp]
+    [gComp.append(e) for e in replaceCourse]
+    return gComp, nComp, tempindex
 
 
 def getWeek(soup):
@@ -180,13 +258,12 @@ def getWeek(soup):
 
 
 def checkMultiple(courseList):
-    temp = []
     for i in range(len(courseList)):
         for j in range(len(courseList)):
             if (i != j):
                 if not courseList[i].isCompatible(courseList[j]):
-                    courseList[i].compatible = False
-    courseList = [e for e in courseList if e.compatible==True]
+                    courseList[i].sameTime.append(j)
+                    courseList[i].sameTime.append(i)
     return courseList
 
 
