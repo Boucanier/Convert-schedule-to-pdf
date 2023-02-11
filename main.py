bb0a28e80b60de563f9a88dc0b2a6517e35a1911
Boucanier <118428520+Boucanier@users.noreply.github.com>
2023-02-11 15:13:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import toXLSX
 import scrapper
 
 
-if __name__ == "__main__":
+if __name__ == "__main__" :
 
     url, title, weekChoice = scrapper.getLink()
     response = scrapper.getSchedule(url)
 
     courseList = scrapper.parseSchedule(response)
 
-    courseW0, courseW1, courseW2, courseW3 = scrapper.sortCourse(courseList)
-
-    courseList = [courseW0, courseW1, courseW2, courseW3]
+    courseList, overCourse, overIndex = scrapper.sortCourse(courseList)
 
     toXLSX.transformToXls(courseList[weekChoice], title)
