193c9385c906d90e4659c86eb8f0e54154262bc8
Jules C <118428520+Boucanier@users.noreply.github.com>
2023-03-08 17:30:41 +0100

    New - Convert a file into a pdf file

---
+++ toPDF.py
@@ -0,0 +1,33 @@
+"""
+    This module will convert a file into a pdf file and open it
+"""
+import platform
+import subprocess
+
+
+def convertToPdf(fileName : str) -> None:
+    '''
+        Convert a file to pdf using a libreOffice command\n
+        Clear the terminal\n
+        Open the previously created pdf
+
+        - Args :
+            - filename (str) : file name with extension
+    '''
+    if platform.system() == "Linux" :
+        subprocess.run('libreoffice --convert-to pdf ' + fileName, shell = True)
+        subprocess.run('clear', shell = True)
+        subprocess.run('xdg-open ' + fileName.split('.')[0] + '.pdf', shell = True)
+        
+    elif platform.system() == "Windows" :
+        subprocess.run('cd > path.txt', shell = True)
+        with open("path.txt", "r") as fl :
+            path = str(fl.readlines()[0][:-1])
+        file_path = path
+        path = '\"' + path + '\"'
+        file_path += '/' + fileName
+        file_path = '\"' + file_path + '\"'
+        subprocess.run('"C:/Program Files/LibreOffice/program/soffice.exe" --convert-to pdf:writer_pdf_Export ' + file_path + ' --outdir ' + path)
+        subprocess.run('del path.txt', shell = True)
+        subprocess.run('cls', shell = True)
+        subprocess.run('start /B ' + fileName.split('.')[0] + '.pdf', shell = True)
