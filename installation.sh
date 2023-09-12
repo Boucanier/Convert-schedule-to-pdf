049a1b1a1b9d4087a25fa179b052dd3f802082bf
boucanier <chironjules.78@gmail.com>
2023-09-12 16:11:31 +0200

    Adapt installation file for externally managed python environment

--- installation.sh
+++ installation.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install python3 libreoffice-calc xdg-utils
-pip install requests bs4 lxml xlsxwriter
+pip install requests bs4 lxml xlsxwriter || sudo apt install python3-requests python3-bs4 python3-lxml python3-xlsxwriter
 clear
 echo "Everything is installed"
 sleep 1
