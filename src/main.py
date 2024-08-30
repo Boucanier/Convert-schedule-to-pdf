18ea5c5b83ef0b36a9f7868015ed6bdce3395516
Boucanier <chironjules.78@gmail.com>
2024-08-29 20:45:24 -0400

    Snake casing

--- src/main.py
+++ src/main.py
@@ -10,17 +10,17 @@ def iut_schedule() :
     """
         Process to get the IUT schedule
     """
-    CHOICE = 0
+    choice = 0
 
     # Get every course from the schedule
     # If the general school schedule is not found, get every schedule
-    IUTurl, IUTtitle = scraper.get_link(True, "IUT")
+    iut_url, iut_title = scraper.get_link(True, "IUT")
 
-    if IUTtitle and IUTurl :
-        allCourse, weekDesc = element_schedule.get_full_schedule(IUTurl, IUTtitle)
-        db_op.overwrite_db(allCourse, weekDesc)
+    if iut_title and iut_url :
+        all_course, week_desc = element_schedule.get_full_schedule(iut_url, iut_title)
+        db_op.overwrite_db(all_course, week_desc)
 
-    while CHOICE != 5 :
+    while choice != 5 :
 
         print("1 Emploi du temps de groupe")
         print("2 Emploi du temps par prof")
@@ -28,66 +28,66 @@ def iut_schedule() :
         print("4 Mise à jour de la base de données")
         print("5 Quitter\n")
 
-        while CHOICE not in (1, 2, 3, 4, 5) :
-            CHOICE = input("Sélectionner une option : ")
-            if CHOICE.isdigit() and (int(CHOICE) in (1, 2, 3, 4, 5)) :
-                CHOICE = int(CHOICE)
+        while choice not in (1, 2, 3, 4, 5) :
+            choice = input("Sélectionner une option : ")
+            if choice.isdigit() and (int(choice) in (1, 2, 3, 4, 5)) :
+                choice = int(choice)
             else :
                 print("Selectionner une option VALIDE\n")
-                CHOICE = 0
+                choice = 0
 
-        if CHOICE == 1 :
+        if choice == 1 :
 
             url, title = scraper.get_link()
             response = scraper.get_schedule(url)
 
-            courseList, weekDesc = scraper.parse_schedule(response)
+            course_list, week_desc = scraper.parse_schedule(response)
 
-            courseList, overCourse = scraper.sort_courses(courseList)
+            course_list, over_course = scraper.sort_courses(course_list)
 
             to_pdf.clear_files('xlsx', 'pdf', path = OUTPUT_DIR)
-            to_xlsx.create_xlsx(courseList,
-                                overCourse,
-                                weekDesc,
+            to_xlsx.create_xlsx(course_list,
+                                over_course,
+                                week_desc,
                                 title,
                                 OUTPUT_DIR + title.replace(' ', '_'))
             to_pdf.convert_to_pdf(OUTPUT_DIR + title.replace(' ', '_') + ".xlsx")
 
-            CHOICE = 0
+            choice = 0
 
-        elif CHOICE in (2, 3, 4) :
-            if CHOICE in (2, 3) :
+        elif choice in (2, 3, 4) :
+            if choice in (2, 3) :
                 options = ("staff", "room")
 
                 # Get the list of all elements from the selected option
-                elementList = element_schedule.get_full_list(allCourse, options[CHOICE - 2])
+                element_list = element_schedule.get_full_list(all_course, options[choice - 2])
 
                 # Ask the user to select an element from the list
-                elementChoice = element_schedule.choose_element(elementList)
+                element_choice = element_schedule.choose_element(element_list)
 
                 # Get the list of all courses of the selected element
-                courseList = element_schedule.get_course_element(elementChoice,
-                                                                allCourse,
-                                                                options[CHOICE - 2])
+                course_list = element_schedule.get_course_element(element_choice,
+                                                                all_course,
+                                                                options[choice - 2])
 
-                courseList = element_schedule.check_equals(courseList)
-                courseList = element_schedule.merge_course(courseList)
-                courseList, overCourse = scraper.sort_courses(courseList)
+                course_list = element_schedule.check_equals(course_list)
+                course_list = element_schedule.merge_course(course_list)
+                course_list, over_course = scraper.sort_courses(course_list)
 
                 to_pdf.clear_files('xlsx', 'pdf', path = OUTPUT_DIR)
-                to_xlsx.create_xlsx(courseList,
-                                    overCourse,
-                                    weekDesc,
-                                    elementChoice,
-                                    OUTPUT_DIR + elementChoice.replace(' ', '_'))
-                to_pdf.convert_to_pdf(OUTPUT_DIR + elementChoice.replace(' ', '_') + ".xlsx")
+                to_xlsx.create_xlsx(course_list,
+                                    over_course,
+                                    week_desc,
+                                    element_choice,
+                                    OUTPUT_DIR + element_choice.replace(' ', '_'))
+                to_pdf.convert_to_pdf(OUTPUT_DIR + element_choice.replace(' ', '_') + ".xlsx")
 
             else :
-                IUTurl, IUTtitle = scraper.get_link(True, "IUT")
-                allCourse, weekDesc = element_schedule.get_full_schedule(IUTurl, IUTtitle)
-                db_op.overwrite_db(allCourse, weekDesc)
+                iut_url, iut_title = scraper.get_link(True, "IUT")
+                all_course, week_desc = element_schedule.get_full_schedule(iut_url, iut_title)
+                db_op.overwrite_db(all_course, week_desc)
 
-            CHOICE = 0
+            choice = 0
 
 
 def uqac_schedule() :
